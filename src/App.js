d73e95aec93205b658e2de97f83301bf1f625586
jukha <Jukka.Hamalainen@wapice.com>
2020-09-24 00:53:07 +0300

    Drawing a rectangle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const canvasRef = React.useRef(null);
+
+  const [context, setContext] = React.useState(null);
+
+  React.useEffect(() => {}, [context]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Heheh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: 'center',
+      }}>
+      <canvas
+        id="canvas"
+        ref={canvasRef}
+        width={500}
+        height={500}
+        style={{
+          border: '2px solid #000',
+          marginTop: 10,
+        }}
+      ></canvas>
     </div>
   );
 }
 
+//export default Canvas
 export default App;
