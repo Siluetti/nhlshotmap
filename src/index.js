a69dcb15e2e9c6786f11caced12070b8a327d566
Siluetti <hamalainen.jukka@gmail.com>
2020-10-10 20:06:44 +0300

    Created folder called redux for Redux related classes and functions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import configureStore from './stores/appStore';
+import configureStore from './redux/stores/appStore';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
